8b47de048884fe72ec5d7da5771ad0e708f5caf4
Iain Shorter <iain.shorter@nanoporetech.com>
2021-07-23 17:29:56 +0100

    rework ts project settings - migrate primary tsconfig to tsconfig.main - create new catchall tsconfig for lint/test - ensure build uses primary tsconfig - removed extra configs from examples

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,27 @@
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 import { dirname } from 'path';
+import { asStruct, isString } from 'ts-runtime-typecheck';
+
+const { main: MAIN, module: MODULE } = asStruct({
+  main: isString,
+  module: isString
+})(pkg);
 
 export default {
 	input: 'src/index.ts',
 	preserveModules: true,
 	plugins: [
-		typescript()
+		typescript({ tsconfig: './tsconfig.main.json' })
 	],
 	output: [
 		{
-			dir: `dist/${dirname(pkg.main)}`,
+			dir: `dist/${dirname(MAIN)}`,
 			entryFileNames: '[name].js',
 			format: 'cjs'
 		},
 		{
-			dir: `dist/${dirname(pkg.module)}`,
+			dir: `dist/${dirname(MODULE)}`,
 			entryFileNames: '[name].mjs',
 			format: 'esm'
 		}
