c634a2550154e4fbb834795e14d96b19ae54a88b
Iain Shorter <iain.shorter@nanoporetech.com>
2022-04-07 17:50:28 +0100

    WIP: doc generation

---
+++ src/generate_docs.ts
@@ -0,0 +1,18 @@
+import { terminal } from "./Terminal"
+
+const print_header = (str: string) => {
+  const h = (terminal.indent / 2) + 1;
+  return `${'#'.repeat(h)} ${str}`;
+}
+export function generate_docs (exe: string, cmd: Command): string {
+
+
+  terminal.print_line(`${}`)
+  command
+
+  description
+  
+  examples
+
+
+}
